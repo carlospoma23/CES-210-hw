1b6060c05c428d6efd9f10783fd361fcfc371e55
carlospoma23 <carlospoma23@gmail.com>
2024-10-01 22:17:16 -0600

    Learning 3 Done

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,65 @@
+public class Fraction
+{
+
+    private int _top;
+    private int _bottom;
+
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+
+    }
+
+
+    public Fraction(int top)
+    {
+        _top = top;
+        _bottom = 1;
+
+
+    }
+
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+
+    }
+
+    //I need to understand Getters and Setters
+
+    /*
+    public int getTop()
+    {
+        return _top;
+    }
+
+    public int setTop(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    */
+
+
+    public string GetFractionString()
+    {
+        string text = $"{_top}/{_bottom}";
+
+
+        return text;
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / (double)_bottom;
+    }
+
+
+
+
+}
